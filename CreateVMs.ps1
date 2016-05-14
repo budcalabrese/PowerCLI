b04da0b8825c3edc198f2f62f718a93523f1fc21
Bud Calabrese <budcalabrese@gmail.com>
2016-05-13 19:18:16 -0500

    Fixed Datastore Variable
    I corrected the variable Get-Datastore $_.Datastore

--- CreateVMs.ps1
+++ CreateVMs.ps1
@@ -13,6 +13,6 @@ Connect-VIServer -Server $vCenter_Server
 $VirtualMachineCSV = "C:\temp\VMTemplate.csv"
 
 $VirtualMachine = Import-CSV $VirtualMachineCSV
-$VirtualMachine | %{ New-VM -Name $_.Name -Template $(Get-Template  $_.Template) -VMHost $(Get-VMHost $_.DestinationHost) -Datastore$(Get-Datastore $_.DestinationHost) -OSCustomizationSpec $(Get-OSCustomizationSpec $_.CustomSpec) }
+$VirtualMachine | %{ New-VM -Name $_.Name -Template $(Get-Template  $_.Template) -VMHost $(Get-VMHost $_.DestinationHost) -Datastore$(Get-Datastore $_.Datastore) -OSCustomizationSpec $(Get-OSCustomizationSpec $_.CustomSpec) }
 $VirtualMachine | %{ Set-VM -VM $_.Name -NumCpu $_.NumCpu -MemoryMB $_.MemoryMB -Confirm:$false }
 $VirtualMachine | %{ Start-VM -VM $_.Name -Confirm:$false }
