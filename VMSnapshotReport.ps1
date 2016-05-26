e3e869d204ff53ea8826a45e4f9c7206d3e60811
Bud Calabrese <budcalabrese@gmail.com>
2016-05-25 21:23:32 -0500

    Added lots of features
    Added:
    -Importing PowerCLI module
    -Connecting to vCenter
    -Added a PSObject if there are no snapshots
    -Cleaned up comments

--- VMSnapshotReport.ps1
+++ VMSnapshotReport.ps1
@@ -1,4 +1,14 @@
-##### PowerCLI Awesome Snapshot Report tool ######
+Write-Host This script requires PowerCLI 6, see notes if you have PowerCLI 5
+##If you have PowerCLI 5 use Add-PSSnapin VMware.VimAutomation.Vds -ErrorAction Stop##
+
+# Imports the PowerCLI module
+Import-Module VMware.VimAutomation.Vds -ErrorAction Stop
+
+# Setting our vCenter server
+$VIServer = "VCENTER.V-BLOG.local"
+
+# Connecting to vCenter and prompts for credentials
+Connect-VIServer $VIServer -Credential (Get-Credential) | Out-Null
 
 # Used to create our HTML output table with CSS
 $Head = @"
@@ -11,8 +21,17 @@ TR:Nth-Child(Even) {Background-Color: #dddddd;}
 </style>
 "@
 
-# Gets snapshot info and reports out the details
+# Checks to see if there are any snapshots
+# If no snapshots the script will generate a report saying no snapshots
+# Else it will generate a report showing the snapshots
 $Report = Get-VM | Get-Snapshot | Select VM,Name,Description,@{Label="Size";Expression={"{0:N2} GB" -f ($_.SizeGB)}},Created
-
-# Converts the output to HTML and save it to c:\temp
-$Report | ConvertTo-Html -Head $Head -PreContent "<p><h2> Snapshot Report </h2></p><br>" | Out-File "C:\temp\SnapShotReport.html"
+If (-not $Report)
+{  $Report = New-Object PSObject -Property @{
+      VM = "No snapshots found on any VM's controlled by $VIServer"
+      Name = ""
+      Description = ""
+      Size = ""
+      Created = ""
+   }
+}
+$Report | ConvertTo-Html -Head $Head -PreContent "<p><h2> Snapshot Report - $VIServer </h2></p><br>" | Out-File "C:\temp\SnapshotReport.html"
